54e5480194c47fc48b066716ca8caf3329a67428
Marcin Majborski <marcinmajborski@gmail.com>
2024-10-10 12:20:35 +0200

    sekcja 7 done

---
+++ 02-TypesAndVariables/7_10.py
@@ -0,0 +1,17 @@
+###
+# A program that enables a user to challenge a computer.
+# The computer throws dice. Then, the user then tries to guess
+# the number from dice by entering a number from 1 to 6
+# from the keyboard. If the user has guessed the number
+# from the dice, the computer displays True. Otherwise,
+# the computer displays False.
+#
+import random
+# COMPUTER TURN
+computer = random.randint(1,6)
+# YOUR TURN
+you = input("Enter your number: ")
+
+is_correct = computer == you
+
+print(f'You won: {is_correct}')
---
+++ 02-TypesAndVariables/7_6.py
@@ -0,0 +1,4 @@
+speed = int(input("Enter vehicle speed: "))
+correct_speed = 40 <= speed and speed <= 140
+
+print(f"Speed is valid: {correct_speed}")
