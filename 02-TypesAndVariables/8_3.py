39198535aa98a5c43d4750f7cd0b4f8f3ed58b6d
Marcin Majborski <marcinmajborski@gmail.com>
2024-10-12 12:12:14 +0200

    sekcja 8 skonczona (8_3?)

--- 02-TypesAndVariables/8_3.py
+++ 02-TypesAndVariables/8_3.py
@@ -2,8 +2,9 @@
 # A program that displays your height both in cm and in feet and inches.
 #
 cm = 170
-feet = 0
-inches = 0
+#poprawić ewentualnie przelicznik
+feet = cm // 27
+inches = cm % 12
 # calculate the number of feet
 
-print(f'I am {cm}cm tall, i.e. ... feet and ... inches')
+print(f'I am {cm}cm tall, i.e. {feet} feet and {inches} inches')
