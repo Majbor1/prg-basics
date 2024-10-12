bbc82c5b267725864e338b0687e4e6387adbf2d0
Marcin Majborski <marcinmajborski@gmail.com>
2024-10-12 19:27:36 +0200

    temat 2 done

--- 02-TypesAndVariables/8_3.py
+++ 02-TypesAndVariables/8_3.py
@@ -1,10 +1,14 @@
-###
-# A program that displays your height both in cm and in feet and inches.
-#
-cm = 170
-#poprawić ewentualnie przelicznik
-feet = cm // 27
-inches = cm % 12
-# calculate the number of feet
+cm = 170 
 
-print(f'I am {cm}cm tall, i.e. {feet} feet and {inches} inches')
+# Constants for conversion
+cm_to_inches = 0.393701
+inches_to_feet = 12
+
+# Convert centimeters to inches
+height_inches = cm * cm_to_inches
+
+# Calculate feet and inches
+feet = height_inches // inches_to_feet
+inches = height_inches % inches_to_feet
+
+print(f'I am {cm}cm tall, i.e. {round(feet, 0)} feet and {round(inches, 0)} inches')
