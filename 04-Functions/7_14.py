fcaeadb3a3b2d823e28f11816db8be10de79f5f9
Marcin Majborski <marcinmajborski@gmai.com>
2024-10-24 21:56:39 +0200

    do 7_15

--- 04-Functions/7_14.py
+++ 04-Functions/7_14.py
@@ -1,7 +1,16 @@
 def f(detector):
     in_room = 0
     for char in detector:
-        if char == '+':
-            in_room += 1
-        elif char == '-':
-            in_room -= 1
+        if in_room < 3:
+            if char == '+':
+                in_room += 1
+            elif char == '-':
+                in_room -= 1
+        else:
+            return True
+    return False
+    
+print(f("+-+++-+---"))
+print(f("+-+-+-+-"))
+print(f("+-++-+--"))
+print(f("+-++-++-+---"))
