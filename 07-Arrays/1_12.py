3c8aa68bff16d5acccac72dbf6d2601c60ecdf72
Marcin Majborski <marcinmajborski@gmai.com>
2024-11-12 13:51:05 +0100

    do 1_15

---
+++ 07-Arrays/1_12.py
@@ -0,0 +1,8 @@
+categories = ["Food", "Transport", "Rent","Entertainment"]
+expenses = [500, 150, 1000, 200]
+
+max_val = expenses.index(max(expenses))
+
+for i in range(len(expenses)):
+    if i == max_val:
+        print(categories[i])
---
+++ test.py
@@ -0,0 +1,13 @@
+a = 2
+
+b = 3
+c = a + b
+d = a + 5 - c
+
+total = ""
+
+for i in range(1, 11):
+    i = str(i)
+    total += i
+
+print(total)
