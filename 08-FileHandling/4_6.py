8d5f66cab4d36cd1487a556e94e7d787f22c4c86
Marcin Majborski <marcinmajborski@gmai.com>
2024-11-19 20:00:48 +0100

    4_6.py

--- 08-FileHandling/4_6.py
+++ 08-FileHandling/4_6.py
@@ -1,22 +1,23 @@
 def f(file_name):
-    with open(file_name, 'r') as file:
-        content = file.read()
+    try:
+        with open(file_name, 'r') as file:
+            content = file.read()
 
-        characters = 0
+            characters = 0
 
-        splited_lines = content.splitlines()
+            splited_lines = content.splitlines()
 
-        for i in splited_lines:
-            for j in splited_lines:
-                characters += 1
-        words = content.split()
+            for i in splited_lines:
+                for j in splited_lines:
+                    characters += 1
+            words = content.split()
 
-        print(f"File name: {file_name}")
-        print(f"Number of lines: {len(splited_lines)}")
-        print(f"Number of characters: {characters}")
-        print(f"Number of words: {len(words)}")
+            print(f"Number of lines: {len(splited_lines)}")
+            print(f"Number of characters: {characters}")
+            print(f"Number of words: {len(words)}")
+    except FileNotFoundError:
+        print(f"File {file_name} doesn't exist!")
         
 
-f("files.txt")
-f("pets.txt")
-f("it_company.csv")
+a = input("File name: ")
+f(a)
