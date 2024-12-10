37fdc64b230f97635f7ec4bf60557d420ff6f879
Marcin Majborski <71440562+Majbor1@users.noreply.github.com>
2024-12-10 13:31:57 +0000

    3 sekcja

--- 11-ObjectOrientedProgramming/3_2.py
+++ 11-ObjectOrientedProgramming/3_2.py
@@ -1,19 +1,21 @@
-# class definition
 class Song:
-   def __...___(self,...,...,...,...):
-      ...
-      ...
-      ...
-      ...
-   def __str.............
-      ...
-      ...
-      ...
+   def __init__(self, performer, title, album, year):
+      self.performer = performer
+      self.title = title
+      self.album = album
+      self.year = year
 
-# object creation
-song1 = ...
-song2 = ...
+   def __str__(self):
+      return f"""Performer: {self.performer}
+Title: {self.title}
+Album: {self.album}
+Year: {self.year}
+---------------------------------"""
 
-## object usage
-print(song1)
-...
+# Creating an instance of the Car class
+song1 = Song("Ed Sheeran","Hearts Don't Break Around Here","Divide",2017)
+song2 = Song("Queen","Bohemian Rhapsody","A Night at the Opera",1975)
+
+# Print the object
+print(song1)  # Output: 2021 Toyota Corolla
+print(song2)  # Output: 2021 Toyota Corolla
