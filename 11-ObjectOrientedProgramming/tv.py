63963043da6784fd85779f54175d233870cc9509
Marcin Majborski <marcinmajborski@gmail.com>
2024-12-18 22:14:30 +0100

    sekcja 3 skonczona

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -4,6 +4,7 @@ class TV:
     def __init__(self):
         self.is_on = False
         self.channels = []
+        self.volume = 0
 
     def turn_off(self):
         self.is_on = False
@@ -13,14 +14,23 @@ class TV:
       
     def show_status(self):
         if self.is_on:
-            print(f"TV is on, channel {self.channel_no}")
+            print(f"TV is on, volume: {self.volume}")
         else:
             print("TV is off")
 
     def set_channels(self, channels_list):
         for channel in channels_list:
             self.channels.append(channel)
-    
 
+    def show_channels(self):
+        print(f"Available channels: {self.channels}")
+
+    def volume_up(self):
+        if self.volume < 10:
+            self.volume += 1
+
+    def volume_down(self):
+        if self.volume > 0:
+            self.volume -= 1
 
 
--- 11-ObjectOrientedProgramming/tv_show.py
+++ 11-ObjectOrientedProgramming/tv_show.py
@@ -3,6 +3,8 @@
 from tv import TV
 
 def main():
+    my_channels = ["TVP1", "TVP2", "Polsat", "TVN", "Filmbox", "Discovery"]
+
     # object creation
     my_tv = TV()
     
@@ -10,7 +12,15 @@ def main():
     my_tv.show_status()
     my_tv.turn_on()
     my_tv.show_status()
-    my_tv.set_channel(5)
+    my_tv.show_channels()
+    my_tv.set_channels(my_channels)
+    my_tv.show_channels()
+    my_tv.volume_up()
+    my_tv.volume_up()
+    my_tv.volume_up()
+    my_tv.volume_up()
+    my_tv.show_status()
+    my_tv.volume_down()
     my_tv.show_status()
     my_tv.turn_off()
     my_tv.show_status()
